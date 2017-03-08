f84fd8a86a00965d9941137fd71016ae91fd746b
Luke Chui <chui.luke99@gmail.com>
2017-03-08 13:04:29 -0800

    completed other soluitons

--- src/balancedParentheses.js
+++ src/balancedParentheses.js
@@ -21,5 +21,30 @@
  *   returns: false
  */
 module.exports = ( input ) => {
+	if (input === null || input === '') return false
 
+	const stack = []
+	let include = false // used to check if string has 
+
+	const pairs = {
+		'(' : ')',
+		'{' : '}',
+		'[' : ']',
+	}
+
+	for (let i = 0; i < input.length; i++) {
+		let curr = input[i]
+		if (pairs[curr]) {
+			stack.push(curr)
+			include = true
+		} else if (curr === ')' || curr === '}' || curr === ']') {
+			let top = stack.pop()
+			if (curr !== pairs[top]) {
+				return false
+			}
+		}
+		console.log('STACK: ', stack)
+	}
+
+	return (stack.length === 0) && include
 };
--- src/deepFinder.js
+++ src/deepFinder.js
@@ -14,5 +14,34 @@
  * 
  */
 module.exports = ( input, test ) => {
+	// always assume input in unsanitized
+	if (input === null || input.length === 0 || test === null) {
+		return null
+	}
 
+	/*
+	 * When checking the variable type in the array, its good to
+	 * note the difference between typeof and instanceof.
+	 *
+	 * Using typeof(obj) will return a string represention of the object type
+	 * while instanceof is a boolean checking operation
+	 */
+
+	let arr = []
+	function deepFind (obj) {
+		if (obj instanceof Array) {
+			obj.forEach((item) => { // recurse through each of array's items
+				deepFind(item)
+			})
+		} else if (obj instanceof Object) {
+			for (let key in obj) { // recurse through each of object's items
+				deepFind(obj[key])
+			}
+		} else if (test(obj)) { // input is neither an Array or Object
+			arr.push(obj)
+		}
+	}
+
+	deepFind(input)
+	return arr
 };
